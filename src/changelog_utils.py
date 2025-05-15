1d2b80e86faeb2a2e268eca3c88877cb51495b06
Sergio Martins <sergio.martins@kdab.com>
2025-05-15 14:45:13 +0100

    changelog_utils.py: Add a parser for GammaRay
    Reuses the one from KDSME which seems generic enough

--- src/changelog_utils.py
+++ src/changelog_utils.py
@@ -3,6 +3,9 @@
 # SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
 # SPDX-License-Identifier: MIT
 
+# example test:
+# ./src/create_release.py --only-print-changelog --repo GammaRay --version 3.2.0 --sha1 master --repo-path ../GammaRay
+
 from utils import download_file_as_string
 
 
@@ -17,8 +20,11 @@ def get_kddockwidgets_changelog(version, sha1):
     return ""
 
 
-def get_kdstatemachineeditor_changelog(version, sha1):
-    filename = f"https://raw.githubusercontent.com/KDAB/KDStateMachineEditor/{sha1}/CHANGES"
+# In lack of better name get_generic_changelog() gets changelog from Gammaray or KDSME
+# If your project has a different changelog format, consider normalizing, or just create
+# a new parser.
+def get_generic_changelog(version, repo, sha1):
+    filename = f"https://raw.githubusercontent.com/KDAB/{repo}/{sha1}/CHANGES"
     text = download_file_as_string(filename)
 
     sections = text.split('Version ')
@@ -34,8 +40,8 @@ def get_changelog(proj_name, version, sha1):
     '''
     if proj_name == 'KDDockWidgets':
         return get_kddockwidgets_changelog(version, sha1)
-    if proj_name == 'KDStateMachineEditor':
-        return get_kdstatemachineeditor_changelog(version, sha1)
+    if proj_name == 'KDStateMachineEditor' or proj_name == 'GammaRay':
+        return get_generic_changelog(version, proj_name, sha1)
 
     raise Exception(
         f"Don't know how to get changelog for project {proj_name}. IMPLEMENT ME")
