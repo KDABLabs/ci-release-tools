aa5424fb0759a15c903d76bb1cfcbe3a3d0b8e20
Sergio Martins <sergio.martins@kdab.com>
2026-02-01 16:07:28 +0000

    Add ci-download-qt.sh
    So CI can download sanitizer Qt's

---
+++ src/build_qt/ci-download-qt.sh
@@ -0,0 +1,39 @@
+
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2026 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+# SPDX-License-Identifier: MIT
+
+# Called by CI to download a sanitizer enabled Qt build, example:
+# ./ci-download-qt.sh 6.11.0 qt-6.11-asan
+
+set -e
+
+error_handler() {
+    local exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "Error: Failed with exit code $exit_code"
+    fi
+}
+
+trap error_handler EXIT
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <tag> <qt package name>"
+    exit 1
+fi
+
+TAG="$1"
+PACKAGE_NAME="$2"
+TARBALL="${PACKAGE_NAME}.tar.zst"
+DEST=~/Qt/${PACKAGE_NAME}
+
+wget -O /tmp/${TARBALL} https://github.com/KDABLabs/ci-release-tools/releases/download/${TAG}/${TARBALL}
+
+rm -rf ${DEST}
+mkdir -p ${DEST}
+
+echo "Extracting Qt package /tmp/${TARBALL} to ${DEST} ..."
+tar -xf /tmp/${TARBALL} -C ${DEST}
+
+rm /tmp/${TARBALL}
